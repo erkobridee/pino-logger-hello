66aaf62c3a3e6f619e7222c5b691338c9fc2c1db
erkobridee <erko.bridee@gmail.com>
2026-05-29 18:44:15 +0200

    update(logger utils): to use the opentelemetry transport

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,38 @@
-import pino, { type Logger } from "pino";
+import type { Logger, TransportTargetOptions } from "pino";
+
+import pino from "pino";
 
 const isBrowser = typeof window !== "undefined";
 const isProduction = process.env.MODE === "production";
+
+interface Transports {
+  targets: TransportTargetOptions[];
+}
+
+const transport: Transports = {
+  targets: [
+    {
+      target: "pino-opentelemetry-transport",
+      options: {
+        "service.name": process.env.OTEL_SERVICE_NAME || "my-node-api",
+        "service.version": "1.0.0",
+        "deployment.environment": process.env.NODE_ENV || "development",
+      },
+    },
+  ],
+};
+
+// Node.js transport (dev only, e.g. Vite SSR or scripts)
+if (!isBrowser && !isProduction) {
+  transport.targets.push({
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      levelFirst: true,
+      translateTime: "SYS:standard",
+    },
+  });
+}
 
 const logger: Logger = pino({
   level: process.env.LOG_LEVEL || "debug",
@@ -22,18 +53,7 @@ const logger: Logger = pino({
     },
   }),
 
-  // Node.js transport (dev only, e.g. Vite SSR or scripts)
-  ...(!isBrowser &&
-    !isProduction && {
-      transport: {
-        target: "pino-pretty",
-        options: {
-          colorize: true,
-          levelFirst: true,
-          translateTime: "SYS:standard",
-        },
-      },
-    }),
+  transport,
 });
 
 export default logger;
