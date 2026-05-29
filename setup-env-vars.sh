4c3f293068f29c6223e4ff41a7f8f844e13d5566
erkobridee <erko.bridee@gmail.com>
2026-05-29 18:46:00 +0200

    feat(project): bash script to setup the needed env vars

---
+++ setup-env-vars.sh
@@ -0,0 +1,4 @@
+export OTEL_SERVICE_NAME="SIMPLE_NODEJS_APP"
+export OTEL_EXPORTER_OTLP_ENDPOINT="https://oneuptime.com/otlp"
+export OTEL_EXPORTER_OTLP_HEADERS="x-oneuptime-token=407356ec-477d-4d4b-b026-6fbcb8ecb1b3"
+export OTEL_EXPORTER_OTLP_PROTOCOL="http/protobuf"
